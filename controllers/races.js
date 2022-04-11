5be60fed470ab80123b56ad6d845189b31e5c6b0
Brandon Neves <brandonneves@brandons-mbp.lan>
2022-04-10 22:03:23 -0400

    Brandon Neves Project 5

--- controllers/races.js
+++ controllers/races.js
@@ -1,8 +1,61 @@
   
 import Race from "../models/race.js";
 
-export const getCharacters = async (req, res) => {};
-export const getCharacter = async (req, res) => {};
-export const createCharacter = async (req, res) => {};
-export const updateCharacter = async (req, res) => {};
-export const deleteCharacter = async (req, res) => {};
+export const getRaces = async (req, res) => {
+  try {
+  const Races = await Race.find();
+  res.json(Races);
+} catch (error) {
+  console.log(error);
+  res.status(500).json({ error: error.message });
+  }
+};
+
+export const getRace = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const race = await Race.findById(id);
+
+    if (arcade) {
+      return res.json(arcade);
+    }
+
+    res.status(400).json({ message: "Race not found!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createRace = async (req, res) => {
+try {
+    const race = new Race(req.body);
+    await race.save();
+    res.status(201).json(race);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateRace = async (req, res) => {
+  const { id } = req.params;
+  const race = await Race.findByIdAndUpdate(id, req.body);
+  res.status(200).json(race);
+};
+
+export const deleteRace = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Race.findByIdAndDelete(id);
+
+    if (deleted) {
+      return res.status(200).send("Race deleted!");
+    }
+
+    throw new Error("Race not found");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
