68c2119ada3b8667c91431124882e620b511efdd
Brandon Neves <brandonneves@brandons-mbp.lan>
2022-04-10 22:12:14 -0400

    Project 5

--- routes/races.js
+++ routes/races.js
@@ -3,10 +3,10 @@ import * as controllers from "../controllers/races.js"
 
 const router = Router();
 
-router.get("/races", races.getCharacters);
-router.get("/races/:id", races.getCharacter);
-router.post("/races", races.createCharacter);
-router.put("/races/:id", races.updateCharacter);
-router.delete("/races/:id", races.deleteCharacter);
+router.get("/races", races.getRaces);
+router.get("/races/:id", races.getRace);
+router.post("/races", races.createRace);
+router.put("/races/:id", races.updateRace);
+router.delete("/races/:id", races.deleteRace);
 
 export default router;
